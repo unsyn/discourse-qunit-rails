e2e0f720be72a9a9ae753d4fe7f46149b2c47eaf
Guo Xiang Tan <tgx_world@hotmail.com>
2016-04-13 10:07:44 +0800

    FIX: Skip precompiled check for qunit-rails assets.

--- lib/qunit/rails/engine.rb
+++ lib/qunit/rails/engine.rb
@@ -7,6 +7,20 @@ module QUnit
       config.qunit.tests_path       = "test"
       config.qunit.javascripts_path = "javascripts"
       config.qunit.stylesheets_path = "stylesheets"
+
+      # https://github.com/rails/sprockets-rails/issues/299#issuecomment-167701012
+      # This doesn't really make sense since the assets are not going to be
+      # available in production.
+      config.after_initialize do
+        ActiveSupport.on_load(:action_view) do
+          default_checker = ActionView::Base.precompiled_asset_checker
+
+          ActionView::Base.precompiled_asset_checker = -> logical_path do
+           default_checker[logical_path] ||
+           %w{qunit.js qunit.css test_helper.css test_helper.js}.include?(logical_path)
+          end
+        end
+      end
     end
   end
 end
